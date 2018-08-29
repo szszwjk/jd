b5cedc062133df13a70b825fa776fd57f6013be4
=szszwjk <704563245@qq.com>
2018-08-29 16:32:39 +0800

    第二次提交

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -6,9 +6,14 @@ import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.content.service.ContentService;
 
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
+import com.taotao.utils.JsonUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -16,6 +21,10 @@ import java.util.List;
 
 @Service
 public class ContentServiceImpl implements ContentService {
+    @Value("${CONTENT_KEY}")
+    private String CONTENT_KEY;
+    @Autowired
+    private JedisClient jedisClient;
     @Autowired
     private TbContentMapper tbContentMapper;
 
@@ -39,14 +48,38 @@ public class ContentServiceImpl implements ContentService {
         tbContent.setUpdated(date);
 
         tbContentMapper.addContent(tbContent);
-
+        //数据变更 清除缓存
+        jedisClient.hdel(CONTENT_KEY,tbContent.getCategoryId()+"");
         return TaotaoResult.ok();
     }
 
     @Override
     public List<TbContent> getContent(long categoryId) {
-        List<TbContent> contentAll = tbContentMapper.findContentAll(categoryId);
+        //这里取缓存
+        try {
+            String json=jedisClient.hget(CONTENT_KEY, categoryId+"");
+            if (StringUtils.isNotBlank(json)){
+                List<TbContent> result = JsonUtils.jsonToList(json, TbContent.class);
+                System.out.println("查询缓存");
+                return result;
 
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        /*
+        * 第一次请求 由于没有缓存 所以直接查询 查询之后在return之前加入redis
+        * 第二次访问 直接查询缓存
+        * */
+        List<TbContent> contentAll = tbContentMapper.findContentAll(categoryId);
+        System.out.println("查询数据库");
+        try {
+            jedisClient.hset(CONTENT_KEY,categoryId+"", JsonUtils.objectToJson(contentAll));
+            System.out.println("加入缓存");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //这里加缓存
         return contentAll;
     }
 }
---
+++ taotao-content/taotao-content-service/src/test/java/com/taotao/test/MyTest.java
@@ -0,0 +1,25 @@
+package com.taotao.test;
+
+import com.sun.javadoc.SeeTag;
+import com.taotao.jedis.JedisClient;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import redis.clients.jedis.HostAndPort;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MyTest {
+    @Test
+    public void demo1()
+    {
+
+    }
+    @Test
+    public void demo2()
+    {
+
+    }
+
+}
